5d7e1ad8db0e53fb129b4805e51f724de7a0615d
Yiqin Zhao <yiqinzhao@outlook.com>
2022-07-08 15:55:34 -0700

    :lipstick: Larger text on md screen

--- layouts/cards.vue
+++ layouts/cards.vue
@@ -1,11 +1,11 @@
 <template>
     <Navigator />
 
-    <article class="px-4 pt-16 md:pt-4 p-4 max-w-prose mx-auto flex flex-row flex-wrap justify-between">
+    <article class="px-4 pt-16 md:pt-4 p-4 max-w-5xl mx-auto flex flex-row flex-wrap justify-between">
         <ContentList v-slot="{ list }" :path="$route.path" :query="{ sort: { date: -1 } }">
-            <div v-for="article in list" :key="article._path" class="w-full rounded-md bg-gray-100 dark:bg-gray-800 prose dark:prose-invert my-4 flex overflow-hidden flex-col
+            <div v-for="article in list" :key="article._path" class="w-full max-w-none rounded-md md:rounded-xl bg-gray-100 dark:bg-gray-800 prose dark:prose-invert my-4 flex overflow-hidden flex-col
                        hover:shadow-lg hover:dark:bg-gray-700 hover:bg-gray-50 [&_img]:hover:opacity-70 transition-[background,shadow]"
-                :class="article.previewCardDirection === 'horizontal' ? 'md:flex-row' : 'flex-col md:w-[47.5%]'">
+                :class="article.previewCardDirection === 'horizontal' ? 'md:flex-row' : 'flex-col md:w-[48%]'">
 
                 <div class="w-full" :class="article.previewCardDirection === 'horizontal' ? 'md:w-1/2' : ''">
                     <a :href="`${article._path}`"><img class="m-0 transition-opacity" :src="article.thumbnail" alt="" /></a>
@@ -16,7 +16,7 @@
                     <div class="p-4 flex flex-col justify-between w-full h-full">
 
                         <div class="uppercase text-xs font-bold opacity-70">{{ article.tag }}</div>
-                        <h1 class="text-bold font-bold text-xl m-0 py-4">{{ article.title }}</h1>
+                        <h1 class="text-bold font-bold text-xl md:text-2xl m-0 py-4">{{ article.title }}</h1>
 
                         <div class="text-xs opacity-70">{{ new Date(article.date).toLocaleDateString() }}</div>
                     </div>
