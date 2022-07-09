e4afb20b07874293e48d926295cbe934ca849a38
Yiqin Zhao <yiqinzhao@outlook.com>
2022-07-08 23:54:58 -0700

    :bug: Fixing a wired effects on safari

--- layouts/cards.vue
+++ layouts/cards.vue
@@ -4,7 +4,7 @@
     <article class="px-4 pt-16 md:pt-4 p-4 max-w-5xl mx-auto flex flex-row flex-wrap justify-between">
         <ContentList v-slot="{ list }" :path="$route.path" :query="{ sort: { date: -1 } }">
             <div v-for="article in list" :key="article._path" class="w-full max-w-none rounded-md md:rounded-xl bg-gray-100 dark:bg-gray-800 prose dark:prose-invert my-4 flex overflow-hidden flex-col
-                       hover:shadow-lg hover:dark:bg-gray-700 hover:bg-gray-50 [&_img]:hover:opacity-70 transition-[background,shadow]"
+                       hover:shadow-lg hover:dark:bg-gray-700 hover:bg-gray-50 [&_img]:hover:opacity-70 transition-all isolate"
                 :class="article.previewCardDirection === 'horizontal' ? 'md:flex-row' : 'flex-col md:w-[48%]'">
 
                 <div class="w-full" :class="article.previewCardDirection === 'horizontal' ? 'md:w-1/2' : ''">
