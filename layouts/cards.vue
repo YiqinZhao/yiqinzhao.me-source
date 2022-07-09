135ad590218c339bce902593c47593b14f2e51fc
Yiqin Zhao <yiqinzhao@outlook.com>
2022-07-09 10:42:04 -0700

    :lipstick: Animation duration updates

--- layouts/cards.vue
+++ layouts/cards.vue
@@ -4,11 +4,11 @@
     <article class="p-4 md:pt-8 max-w-5xl mx-auto flex flex-row flex-wrap justify-between">
         <ContentList v-slot="{ list }" :path="$route.path" :query="{ sort: { date: -1 } }">
             <div v-for="article in list" :key="article._path" class="w-full max-w-none rounded-md md:rounded-xl bg-gray-100 dark:bg-gray-800 prose dark:prose-invert mb-8 flex overflow-hidden flex-col
-                       hover:shadow-lg hover:dark:bg-gray-700 hover:bg-gray-50 [&_img]:hover:opacity-70 [&_img]:hover:scale-105 transition-background isolate"
+                       hover:shadow-lg hover:dark:bg-gray-700 hover:bg-gray-50 [&_img]:hover:opacity-70 [&_img]:hover:scale-[1.02] transition-background isolate duration-500"
                 :class="article.previewCardDirection === 'horizontal' ? 'md:flex-row' : 'flex-col md:w-[48%]'">
 
                 <div class="w-full" :class="article.previewCardDirection === 'horizontal' ? 'md:w-1/2' : ''">
-                    <a :href="`${article._path}`"><img class="m-0 transition-[opacity,transform]" :src="article.thumbnail" alt="" /></a>
+                    <a :href="`${article._path}`"><img class="m-0 transition-[opacity,transform] duration-500" :src="article.thumbnail" alt="" /></a>
                 </div>
 
                 <a :href="`${article._path}`" class="h-full decoration-transparent"
